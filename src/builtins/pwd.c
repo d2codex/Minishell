f0794d452d717b729fb28800cc38c95bebe10415
Pauline Froidure <pafroidu@student.42.fr>
2025-08-27 16:10:12 +0200

    docs(builtin): add documentation for pwd.c
    - add Doxygen like documentation for builtin_pwd

--- src/builtins/pwd.c
+++ src/builtins/pwd.c
@@ -6,20 +6,24 @@
 /*   By: pafroidu <pafroidu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/26 18:18:35 by pafroidu          #+#    #+#             */
-/*   Updated: 2025/08/26 18:21:06 by pafroidu         ###   ########.fr       */
+/*   Updated: 2025/08/27 16:08:20 by pafroidu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-/*
-	NOTE: pwd ignores all argument given and will print the current working
-	directory path anyway
-
-	TODO:
-	- better tests
-	- light makefiles
-	- search for weird edges cases
+/**
+* @brief Implements the pwd builtin command
+*
+* Prints the absolute pathname of the current working directory to stdout.
+* Uses getcwd() system call to get the real current directory, ignoring
+* the $PWD environment variable (like bash does).
+*
+* @param args Command arguments (unused, pwd takes no arguments)
+* @return EXIT_SUCCESS (0) on success, EXIT_FAILURE (1) on error
+*
+* @note Memory is automatically allocated by getcwd(NULL, 0) and freed
+* @note Errors are handled with perror() which displays appropriate message
 */
 int	builtin_pwd(char **args)
 {
