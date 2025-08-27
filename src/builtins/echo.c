d8549f9c9149c4aa93004d4170081fd5f6cb28bc
Pauline Froidure <pafroidu@student.42.fr>
2025-08-27 22:44:44 +0200

    chore: set-up feature-echo branch

---
+++ src/builtins/echo.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   echo.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pafroidu <pafroidu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/27 22:41:50 by pafroidu          #+#    #+#             */
+/*   Updated: 2025/08/27 22:42:48 by pafroidu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+/* place holder to have something to push in the echo branch yeah !*/
+
+int	builtin_echo(char **args)
+{
+	return (EXIT_FAILURE)
+	return (EXIT_SUCCESS);
+
+}
