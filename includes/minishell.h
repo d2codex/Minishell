9af3a92a40264aba8ba8aaaaa3c16a69356f8af6
d2 <d2.de.sousa@gmail.com>
2025-09-19 08:55:53 +0200

    fix(builtin/unset): missing space in function prototype

--- includes/minishell.h
+++ includes/minishell.h
@@ -138,7 +138,7 @@ int			builtin_exit(char **tokens, t_shell *data);
 
 /* src/builtins/unset.c */
 int			remove_env_node(t_list **env_list, const char *token);
-int			builtin_unset(char**tokens, t_shell *data);
+int			builtin_unset(char **tokens, t_shell *data);
 
 /* =========================== */
 /*     ENVIRONMENT IMPORT      */
